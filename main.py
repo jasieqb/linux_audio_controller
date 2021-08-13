cb74f450fba6679c9985d8bd651c8ff272f651a3
Jan Bylina <jfbylina@gmail.com>
2021-08-14 00:25:42 +0200

    work around read from serial, make some beauty changes

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
 import serial
+#import pulsectl
 
 
-def read_from_serial():
+def read_from_serial(ser):
+    line = ser.readline()
+    # except serial.serialutil.SerialException:
+    split_line = line.decode()[:len(line)-2].split('|')  # from 0 to 1023
+    # print(line)
+    return split_line 
+
+
+def main():
     ser = serial.Serial(port='/dev/ttyUSB0')
     ser.readline()
     while True:
-        line = ser.readline()
-        # except serial.serialutil.SerialException:
-        split_line = line.decode()[:len(line)-2].split('|')
-        # print(line)
-        print(split_line)
+        print(read_from_serial(ser))
 
 
-read_from_serial()
+if __name__ == "__main__":
+    main()
