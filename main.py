e3e62abb4c7bb5d915da37c9031c8cd08f117dc3
Jan Bylina <jfbylina@gmail.com>
2021-08-14 15:05:06 +0200

    convert val into percent

--- main.py
+++ main.py
@@ -2,19 +2,24 @@ import serial
 #import pulsectl
 
 
+def values_into_percent(value, max_val):
+    return value/max_val
+
+
 def read_from_serial(ser):
     line = ser.readline()
     # except serial.serialutil.SerialException:
     split_line = line.decode()[:len(line)-2].split('|')  # from 0 to 1023
     # print(line)
-    return split_line 
+    return split_line
 
 
 def main():
     ser = serial.Serial(port='/dev/ttyUSB0')
     ser.readline()
     while True:
-        print(read_from_serial(ser))
+        line = read_from_serial(ser)
+        print(line)
 
 
 if __name__ == "__main__":
